9345618de28ee71bb2a067f56d2c71e484977497
aSeddik93 <seddikabdou@gmail.com>
2017-05-15 12:40:21 +0200

    Stef revision, with sheriff

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -4,4 +4,5 @@ package model;
  * Created by aSeddik on 13-May-17.
  */
 public interface Employee {
+    public boolean identify(String username, int password);
 }
--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,14 +1,36 @@
-package sample;
+package view;
 
+import databaseConnection.Staff;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import model.Employee;
 
+import java.beans.EventHandler;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
 
+    Employee actor;
+    @FXML
+    Button signInButton;
+    @FXML
+    TextField signInTextField;
+    @FXML
+    PasswordField signInPasswordField;
+    @FXML
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Staff staff= new Staff();
 
+    }
+
+    public Employee signIn(Event event){
+                actor=staff.
     }
 }
